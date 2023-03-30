05c6252f629bbbf28e82c864de6863553510ba90
Dondo Dano <oshawott.jr@gmail.com>
2023-03-30 14:45:19 +0800

    updated dashboard

--- resources/views/livewire/dashboard/index.blade.php
+++ resources/views/livewire/dashboard/index.blade.php
@@ -1,145 +1,54 @@
 <div>
+    <!-- Document Count -->
     <div class="row">
-        <div class="col-xl-3 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Completed <br/><small class="text-muted">(Research)</small>
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class='bx bx-check-square dash-icon '></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-3 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                On-Going <br/><small class="text-muted">(Research)</small>
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class='bx bx-trending-up  dash-icon'></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-3 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Presented <br/><small class="text-muted">(Research)</small>
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class='bx bx-bar-chart-square dash-icon'></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-3 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Publication <br/><small class="text-muted">(Research)</small>
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class='bx bx-book-bookmark  dash-icon' ></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-xl-3 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                Organized <br/><small class="text-muted">(Extension)</small>
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class='bx bx-spreadsheet dash-icon' ></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-3 mb-4">
-            <div class="card border-left-primary shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
-                                Linkages <br/><small class="text-muted">(Extension)</small>
-                            </div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
-                        </div>
-                        <div class="col-auto">
-                            <i class='bx bx-extension dash-icon' ></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-3 mb-4">
+        <div class="col-xl-4 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                No. of Training <br/><small class="text-muted">(Extension)</small>
+                                Research
                             </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
                         </div>
                         <div class="col-auto">
-                            <i class='bx bx-layer dash-icon' ></i>
+                            <i class='bx bx-search-alt dash-icon'></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="col-xl-3 mb-4">
+        <div class="col-xl-4 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                No. of Trainees <br/><small class="text-muted">(Extension)</small>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Publication
                             </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
                         </div>
                         <div class="col-auto">
-                            <i class='bx bx-group dash-icon' ></i>
+                            <i class='bx bx-file dash-icon'></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-4 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Presentation
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class='bx bx-slideshow dash-icon' ></i>
                         </div>
                     </div>
                 </div>
@@ -148,7 +57,187 @@
     </div>
 
     <div class="row">
+        <div class="col-xl-4 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Training
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class='bx bx-run dash-icon'></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-4 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Extension
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class='bx bx-chalkboard dash-icon'></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-4 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Partnership
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
+                        </div>
+                        <div class="col-auto">
+                            <i class='bx bx-trip dash-icon'></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/ Document Count -->
+
+
+    <div class="row mb-4">
+
+        <!-- Activity Timeline -->
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header  d-flex align-items-center justify-content-between">
+                    <h5 class="card-title m-0 me-2"><i class="bx bx-list-ul me-2"></i>Activity Timeline</h5>
+                </div>
+                <div class="card-body">
+
+                    <ul class="timeline timeline-dashed ">
+                        <li class="mb-4 timeline-item timeline-item-primary">
+                            <span class="timeline-indicator timeline-indicator-primary">
+                                <i class='bx bx-file'></i>
+                            </span>
+                            <div class="timeline-event">
+                                <div class="timeline-header mb-0">
+                                    <h6 class="mb-0">User Name</h6>
+                                    <a href="/evaluate/1" class="btn p-0" type="button" title="Evaluate">
+                                        <i class='bx bx-comment-dots'></i>
+                                    </a>
+                                </div>
+                                <p class="mb-0">Activity done by user</p>
+                                <small>6 days ago</small>
+                            </div>
+                        </li>
+
+                        <li class="timeline-end-indicator">
+                            <i class="bx bx-check-circle text-success"></i>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+        <!--/ Activity Timeline -->
+
+        <!-- Online Users -->
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header d-flex align-items-center justify-content-between">
+                    <h5 class="card-title  m-0 me-2">Online Users</h5>
+                    <small>2 online</small>
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-borderless">
+                        <thead>
+                            <tr>
+                                <td>Name</td>
+                                <td>Period</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div class="d-flex justify-content-start align-items-center">
+                                        <div class="avatar me-2 avatar-online">
+                                            <span class="avatar-initial rounded-circle bg-label-danger">pi</span>
+                                        </div>
+                                        <div class="d-flex flex-column">
+                                            <h6 class="mb-0 text-truncate">Nathan Wagner</h6>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td><small class="text-truncate text-muted">4 hours ago</small></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="d-flex justify-content-start align-items-center">
+                                        <div class="avatar me-2 avatar-online">
+                                            <span class="avatar-initial rounded-circle bg-label-danger">pi</span>
+                                        </div>
+                                        <div class="d-flex flex-column">
+                                            <h6 class="mb-0 text-truncate">Nathan Wagner</h6>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td><small class="text-truncate text-muted">4 hours ago</small></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!--/ Online Users -->
 
     </div>
 
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title">Document uploaded</h5>
+                </div>
+                <div class="tabl-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>User</th>
+                                <th>Name</th>
+                                <th>Type</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div class="d-flex justify-content-start align-items-center">
+                                        <div class="avatar me-2">
+                                            <span class="avatar-initial rounded-circle bg-label-danger">pi</span>
+                                        </div>
+                                        <div class="d-flex flex-column">
+                                            <h6 class="mb-0 text-truncate">Nathan Wagner</h6>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>Banana Harvesting</td>
+                                <td>Research</td>
+                                <td><span class="badge bg-label-secondary">03-30-2023</span></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
