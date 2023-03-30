a3147a646123064afe13080fd6c51e8063705a82
Dondo Dano <oshawott.jr@gmail.com>
2023-03-30 12:39:48 +0800

    updated dashboard

--- resources/views/livewire/dashboard/index.blade.php
+++ resources/views/livewire/dashboard/index.blade.php
@@ -1,7 +1,6 @@
 <div>
-
     <div class="row">
-        <div class="col-xl-4 col-md-6 mb-4">
+        <div class="col-xl-3 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -19,7 +18,7 @@
             </div>
         </div>
 
-        <div class="col-xl-4 col-md-6 mb-4">
+        <div class="col-xl-3 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -30,14 +29,14 @@
                             <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
                         </div>
                         <div class="col-auto">
-                            <i class='bx bx-check-square dash-icon '></i>
+                            <i class='bx bx-trending-up  dash-icon'></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="col-xl-4 col-md-6 mb-4">
+        <div class="col-xl-3 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -48,16 +47,14 @@
                             <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
                         </div>
                         <div class="col-auto">
-                            <i class='bx bx-check-square dash-icon '></i>
+                            <i class='bx bx-bar-chart-square dash-icon'></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="row">
-        <div class="col-xl-4 col-md-6 mb-4">
+        <div class="col-xl-3 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -68,14 +65,16 @@
                             <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
                         </div>
                         <div class="col-auto">
-                            <i class='bx bx-check-square dash-icon '></i>
+                            <i class='bx bx-book-bookmark  dash-icon' ></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="col-xl-4 col-md-6 mb-4">
+    <div class="row">
+        <div class="col-xl-3 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -86,14 +85,14 @@
                             <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
                         </div>
                         <div class="col-auto">
-                            <i class='bx bx-check-square dash-icon '></i>
+                            <i class='bx bx-spreadsheet dash-icon' ></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="col-xl-4 col-md-6 mb-4">
+        <div class="col-xl-3 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -104,16 +103,14 @@
                             <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
                         </div>
                         <div class="col-auto">
-                            <i class='bx bx-check-square dash-icon '></i>
+                            <i class='bx bx-extension dash-icon' ></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="row">
-        <div class="col-xl-4 col-md-6 mb-4">
+        <div class="col-xl-3 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -124,14 +121,14 @@
                             <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
                         </div>
                         <div class="col-auto">
-                            <i class='bx bx-check-square dash-icon '></i>
+                            <i class='bx bx-layer dash-icon' ></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="col-xl-4 col-md-6 mb-4">
+        <div class="col-xl-3 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
@@ -142,12 +139,16 @@
                             <div class="h5 mb-0 font-weight-bold text-gray-800 dash-count">1</div>
                         </div>
                         <div class="col-auto">
-                            <i class='bx bx-check-square dash-icon '></i>
+                            <i class='bx bx-group dash-icon' ></i>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="row">
+
     </div>
 
 </div>
