e36e428c0105c619f5e8f30b9149b9c34d1d24a5
Dondo Dano <oshawott.jr@gmail.com>
2023-05-03 17:32:54 +0800

    optimized misc seed

--- database/seeders/MiscellaneousSeeder.php
+++ database/seeders/MiscellaneousSeeder.php
@@ -14,57 +14,64 @@ class MiscellaneousSeeder extends Seeder
      */
     public function run()
     {
-        Misc::create([
-            'term' => 'Internally Funded',
-            'group' => 'fundclass'
-        ],[
-            'term' => 'Externally Funded',
-            'group' => 'fundclass'
-        ],[
-            'term' => 'Research',
-            'group' => 'projectcategory'
-        ],[
-            'term' => 'Development',
-            'group' => 'projectcategory'
-        ],[
-            'term' => 'On-Going',
-            'group' => 'projectstatus'
-        ],[
-            'term' => 'Completed',
-            'group' => 'projectstatus'
-        ],[
-            'term' => 'Publish',
-            'group' => 'submissionstatus'
-        ],[
-            'term' => 'Draft',
-            'group' => 'submissionstatus'
-        ],[
-            'term' => 'International',
-            'group' => 'presentationtype'
-        ],[
-            'term' => 'National',
-            'group' => 'presentationtype'
-        ],[
-            'term' => 'Regional',
-            'group' => 'presentationtype'
-        ],[
-            'term' => 'Local',
-            'group' => 'presentationtype'
-        ],[
-            'term' => 'Poor',
-            'group' => 'relevance'
-        ],[
-            'term' => 'Fair',
-            'group' => 'relevance'
-        ],[
-            'term' => 'Satisfactory',
-            'group' => 'relevance'
-        ],[
-            'term' => 'Very Satisfactory',
-            'group' => 'relevance'
-        ],[
-            'term' => 'Excellent',
-            'group' => 'relevance'
-        ])->save();
+        $datas = [
+            [
+                'term' => 'Internally Funded',
+                'group' => 'fundclass'
+            ],[
+                'term' => 'Externally Funded',
+                'group' => 'fundclass'
+            ],[
+                'term' => 'Research',
+                'group' => 'projectcategory'
+            ],[
+                'term' => 'Development',
+                'group' => 'projectcategory'
+            ],[
+                'term' => 'On-Going',
+                'group' => 'projectstatus'
+            ],[
+                'term' => 'Completed',
+                'group' => 'projectstatus'
+            ],[
+                'term' => 'Publish',
+                'group' => 'submissionstatus'
+            ],[
+                'term' => 'Draft',
+                'group' => 'submissionstatus'
+            ],[
+                'term' => 'International',
+                'group' => 'presentationtype'
+            ],[
+                'term' => 'National',
+                'group' => 'presentationtype'
+            ],[
+                'term' => 'Regional',
+                'group' => 'presentationtype'
+            ],[
+                'term' => 'Local',
+                'group' => 'presentationtype'
+            ],[
+                'term' => 'Poor',
+                'group' => 'relevance'
+            ],[
+                'term' => 'Fair',
+                'group' => 'relevance'
+            ],[
+                'term' => 'Satisfactory',
+                'group' => 'relevance'
+            ],[
+                'term' => 'Very Satisfactory',
+                'group' => 'relevance'
+            ],[
+                'term' => 'Excellent',
+                'group' => 'relevance'
+            ]
+        ];
+
+        foreach($datas as $data){
+            Misc::create($data)->save();
+        }
+
     }
 }
