88a0c8b335b58edb6433fab5e52dd074b6031ac8
Dondo Dano <oshawott.jr@gmail.com>
2023-05-12 13:15:36 +0800

    updated migration requisite_responsibility_center

--- database/migrations/2023_03_01_063643_create_requisite_responsibility_center_table.php
+++ database/migrations/2023_03_01_063643_create_requisite_responsibility_center_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateResponsibilityCenterTable extends Migration
+class CreateRequisiteResponsibilityCenterTable extends Migration
 {
     /**
      * Run the migrations.
